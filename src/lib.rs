9a7a74b1f6240128ccb2ecae5cff534a78c0ef8c
Amrit Ghimire <mail@amritghimire.com>
2022-09-03 11:13:47 +0545

    Fix lint and cargo deny

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-use axum::{
-    routing::get,
-    http::StatusCode,
-    response::IntoResponse,
-    Router,
-};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 use std::net::SocketAddr;
 
 pub async fn run() {
@@ -19,13 +14,10 @@ pub async fn run() {
 }
 
 pub fn app() -> Router {
-    Router::new()
-        .route("/health_check", get(health_check))
-
+    Router::new().route("/health_check", get(health_check))
 }
 
-// Health check handler 
+// Health check handler
 async fn health_check() -> impl IntoResponse {
     (StatusCode::OK, "Ok")
 }
-
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -10,13 +10,17 @@ async fn health_check_works() {
     let app = zero2prod::app();
 
     let response = app
-            .oneshot(Request::builder().uri("/health_check").body(Body::empty()).unwrap())
-            .await
-            .unwrap();
+        .oneshot(
+            Request::builder()
+                .uri("/health_check")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
 
     assert_eq!(response.status(), StatusCode::OK);
 
     let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
     assert_eq!(&body[..], b"Ok");
 }
-
